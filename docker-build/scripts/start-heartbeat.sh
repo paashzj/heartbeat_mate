97b0e737a6e386ee229d8041d25b5f6c9e7a22bd
shoothzj <shoothzj@gmail.com>
2021-08-24 21:43:00 +0800

    Fix console log path

--- docker-build/scripts/start-heartbeat.sh
+++ docker-build/scripts/start-heartbeat.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-nohup $HEARTBEAT_HOME/heartbeat -e -c $HEARTBEAT_HOME/heartbeat.yml >$PACKETBEAT_HOME/heartbeat.stdout.log 2>$PACKETBEAT_HOME/heartbeat.stderr.log &
+nohup $HEARTBEAT_HOME/heartbeat -e -c $HEARTBEAT_HOME/heartbeat.yml >$HEARTBEAT_HOME/heartbeat.stdout.log 2>$HEARTBEAT_HOME/heartbeat.stderr.log &
