5e01cc84a00381c94f1fdeb1b75e3051806907b6
lovehzj <1922919664@qq.com>
2022-01-09 14:53:13 +0800

    fix mate log path

--- docker-build/scripts/start-daemon.sh
+++ docker-build/scripts/start-daemon.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
+mkdir $HEARTBEAT_HOME/logs
+nohup $HEARTBEAT_HOME/mate/heartbeat_mate >>$HEARTBEAT_HOME/logs/heartbeat_mate.stdout.log 2>>$HEARTBEAT_HOME/logs/heartbeat_mate.stderr.log
 
-nohup $HEARTBEAT_HOME/mate/heartbeat_mate >$HEARTBEAT_HOME/heartbeat_mate.stdout.log 2>$HEARTBEAT_HOME/heartbeat_mate.stderr.log
