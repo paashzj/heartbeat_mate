9fca3c5aadde92a316b0108dfeb8eaef6f9e8d67
lovehzj <1922919664@qq.com>
2022-01-09 19:24:07 +0800

    fix kernel log path

--- docker-build/scripts/start-heartbeat.sh
+++ docker-build/scripts/start-heartbeat.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-nohup $HEARTBEAT_HOME/heartbeat -e -c $HEARTBEAT_HOME/heartbeat.yml >>$HEARTBEAT_HOME/heartbeat.stdout.log 2>>$HEARTBEAT_HOME/heartbeat.stderr.log &
+mkdir $HEARTBEAT_HOME/logs
+nohup $HEARTBEAT_HOME/heartbeat -e -c $HEARTBEAT_HOME/heartbeat.yml >>$HEARTBEAT_HOME/logs/heartbeat.stdout.log 2>>$HEARTBEAT_HOME/logs/heartbeat.stderr.log &
+
