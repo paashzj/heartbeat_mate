a177f522445461b06f4f5884292307a2c8e12d3d
lovehzj <1922919664@qq.com>
2022-01-14 20:10:21 +0800

    Release docker image binary

--- docker-build/scripts/start-daemon.sh
+++ docker-build/scripts/start-daemon.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
-mkdir $HEARTBEAT_HOME/logs
+mkdir -p $HEARTBEAT_HOME/logs
 nohup $HEARTBEAT_HOME/mate/heartbeat_mate >>$HEARTBEAT_HOME/logs/heartbeat_mate.stdout.log 2>>$HEARTBEAT_HOME/logs/heartbeat_mate.stderr.log
+
 
--- docker-build/scripts/start-heartbeat.sh
+++ docker-build/scripts/start-heartbeat.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-mkdir $HEARTBEAT_HOME/logs
+mkdir -p $HEARTBEAT_HOME/logs
 nohup $HEARTBEAT_HOME/heartbeat -e -c $HEARTBEAT_HOME/heartbeat.yml >>$HEARTBEAT_HOME/logs/heartbeat.stdout.log 2>>$HEARTBEAT_HOME/logs/heartbeat.stderr.log &
 
